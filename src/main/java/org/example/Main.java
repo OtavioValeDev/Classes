d1ea6c03791760be263f8ef1cc7a7f06c0fdb894
Otávio Augusto <otavioaugusto_2007@outlook.com>
2025-10-11 21:00:29 -0300

    fix: arrumando o erro de branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,14 @@
 package org.example;
 
-import classes.Cafe;
-import classes.Cachorro;
-import classes.*; //traz tudo, diferente dos demais acima que só pego o necessário. siga os de cima, são uma boa pratica
-
-//import Classes.Cachorro;
+//import org.example.Cachorro;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //chamando usando construtores
-//        Cachorro cachorro = new Cachorro("Lucas", "Pintcher", "Marrom", 2);
-//        cachorro.latir();
-//        cachorro.mostrarDados();
+public static void main(String[] args) {
+/*Cachorro cachorro = new Cachorro("Lucas", "Pintcher", "Marrom", 2);
+       cachorro.latir();
+       cachorro.mostrarDados();*/
 
-        //chamando pelo atributo, só não esqueça de comentar o construtor
 //        Cachorro c1 = new Cachorro();
 //        c1.nome = "Pedro";
 //        c1.raca = "Pastor Alemão";
@@ -23,38 +17,36 @@ public class Main {
 //        c1.latir();
 //        c1.mostrarDados();
 
-        //chamando pelos metodos get/set e herança, só não esqueça de comentar o construtor
-        Cachorro c2 = new Cachorro();
-        c2.setNome("Luisa");
+        Cafe cafe= new Cafe();
+        cafe.marcaCafe();
+        cafe.vaiAcucar();
+        cafe.quantidade();
+        cafe.mostraDados();
+//        Cachorro c2 = new Cachorro();
+//        c2.setNome("Luisa");
+//        c2.getNome();
 //        c2.setRaca("Shitzu");
+//        c2.getRaca();
 //        c2.setCor("Branco");
-        c2.setIdade(2);
+//        c2.getCor();
+//        c2.setIdade(2);
+//        c2.getIdade();
 //        System.out.println("Nome:" + c2.getNome() + ",\n" + "Raça:" + c2.getRaca() + ",\n" + "Cor:" + c2.getCor() + ",\n" + "Idade:" + c2.getIdade() + "\n");
-        c2.emitirSom();
-        c2.alimentar();
-        c2.mostrarDados();
 
-        //chamando a classe gato que herda de animal
-        Gato gato = new Gato("Mingau", 3);
-        gato.emitirSom();
-        gato.alimentar();
 
-        //via metodos, só não esqueça de comentar o construtor
 //        Cafe cafe = new Cafe();
 //        cafe.marcaCafe();
 //        cafe.vaiAcucar();
 //        cafe.quantidade();
 //        cafe.mostraDados();
 
-        //via get e set, só não esqueça de comentar o construtor
-//        Cafe cafe2= new Cafe();
-//        cafe2.newMarcaCafe(3);
-//        cafe2.setAcucar('s');
-//        cafe2.setQuantidade(600);
-//        cafe2.mostraDados();
-
-        //via construtor
-//        Cafe melitta= new Cafe('s',500,2);
-//        melitta.mostraDados();
-    }
+        Cafe cafe2= new Cafe();
+//        cafe2.marcaCafe();
+        cafe2.newMarcaCafe(3);
+//        cafe2.vaiAcucar();
+        cafe2.setAcucar('s');
+//        cafe2.quantidade();
+        cafe2.setQuantidade(600);
+        cafe2.mostraDados();
+}
 }
